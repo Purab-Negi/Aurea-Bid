baaa1fac10290ae637606d33e7990409539e5e0d
Purab Negi <1609purab@gmail.com>
2025-10-17 12:36:12 +0530

    feat: add User schema with validation and basic structure

---
+++ Backend/Models/userSchema.js
@@ -0,0 +1,64 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const userSchema = new mongoose.Schema({
+  userName: {
+    type: String,
+    minLength: [3, "Username should contain at least 3 characters"],
+    maxLength: [50, "Username cannot exceed 50 characters"],
+  },
+  password: {
+    type: String,
+    select: false,
+    minLength: [8, "Password must contain at least 8 characters"],
+    maxLength: [18, "Password cannot exceed 18 characters"],
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/^\S+@\S+\.\S+$/, "Please enter a valid email address"],
+  },
+  address: String,
+  phone: {
+    type: String,
+    minLength: [10, "Phone Number must contain at least 10 digits"],
+    maxLength: [10, "Phone Number cannot exceed 10 digits"],
+  },
+  profileImage: {
+    public_id: { type: String, required: true },
+    url: {
+      type: String,
+      required: true,
+    },
+  },
+  paymentMethods: {
+    bankTransfer: {
+      bankAccountNumber: String,
+      bankAccountName: String,
+      bankName: String,
+    },
+    UPI: {
+      upiId: String,
+    },
+    paypal: {
+      paypalEmail: String,
+    },
+  },
+  role: {
+    type: String,
+    enum: ["Auctioneer", "Bidder", "Super Admin"],
+  },
+  unpaidCommission: {
+    type: Number,
+    default: 0,
+  },
+  moneySpent: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: { type: Date, default: Date.now() },
+});
+
+export const User = mongoose.model("User", userSchema);
