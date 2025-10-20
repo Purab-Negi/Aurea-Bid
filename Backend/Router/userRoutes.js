10921f10cd9ed9b3c805405b5e772cb0418009a5
Purab Negi <1609purab@gmail.com>
2025-10-20 12:08:41 +0530

    Added user login feature with JWT authentication and error handling

--- Backend/Router/userRoutes.js
+++ Backend/Router/userRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { register } from "../Controllers/userController.js";
+import { login, register } from "../Controllers/userController.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/register", register);
+userRouter.post("/login", login);
 
 export default userRouter;
