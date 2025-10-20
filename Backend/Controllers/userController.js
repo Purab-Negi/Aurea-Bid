e8cfb66f5899fc3f504b0cf1f66bad21750811b4
Purab Negi <1609purab@gmail.com>
2025-10-20 13:24:03 +0530

    feat: added authentication middleware and get user profile route

--- Backend/Controllers/userController.js
+++ Backend/Controllers/userController.js
@@ -108,3 +108,8 @@ export const login = catchAsyncErrors(async (req, res, next) => {
   }
   generateToken(user, "Login successful", 200, res);
 });
+
+export const getProfile = catchAsyncErrors(async (req, res, next) => {
+  const user = req.user;
+  res.status(200).json({ success: true, user });
+});
--- Backend/Router/userRoutes.js
+++ Backend/Router/userRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { login, register } from "../Controllers/userController.js";
+import { getProfile, login, register } from "../Controllers/userController.js";
+import { isAuthenticated } from "../middlewares/auth.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/register", register);
 userRouter.post("/login", login);
+userRouter.get("/me", isAuthenticated, getProfile);
 
 export default userRouter;
