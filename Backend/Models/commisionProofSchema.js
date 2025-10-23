fca1a4bc457132d0b595111eeba0d7519f87c0a5
Purab Negi <1609purab@gmail.com>
2025-10-23 15:49:56 +0530

    feat: add PaymentProof model for manual payment handling

---
+++ Backend/Models/commisionProofSchema.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+const paymentProof = mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  proof: {
+    public_id: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+  },
+  uploadedAt: { type: Date, default: Date.now() },
+  status: {
+    type: String,
+    default: "Pending",
+    enum: ["Pending", "Approved", "Rejected", "Settled"],
+  },
+  amount: Number,
+  comment: String,
+});
+
+export const PaymentProof = mongoose.model("PaymentProof", paymentProof);
